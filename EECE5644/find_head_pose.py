db7a884b1a89da588acdc8528cbc9d2fd0e7c8a1
TMLMNG <26241341+TMLMNG@users.noreply.github.com>
2018-06-19 23:21:21 -0400

    Fixed angle extraction from filename.

--- EECE5644/find_head_pose.py
+++ EECE5644/find_head_pose.py
@@ -23,14 +23,15 @@ file_name = fd.askopenfilename()
 print("\nPath:\n" + file_name + '\n')
 image_name = os.path.basename(file_name)
 
-# Extract filename data (TODO)
-verti_angle = re.search(r'(?<=(-|\+))\w+', image_name).group(0)
-horiz_angle = re.search(r'(?<=(-|\+))\w+', image_name).group(0)
+# Extract filename data
+angles = re.compile(r"[+-]?\d+(?:\.\d+)?")
+verti_angle = angles.findall(image_name)[1]
+horiz_angle = angles.findall(image_name)[2]
 print("Image Vertical Angle:   " + verti_angle)
 print("Image Horizontal Angle: " + horiz_angle)
 
 # Process image with OpenCV2
-print("\nProcessing image ..... " + image_name + '\n')
+print("\nProcessing image: " + image_name + '\n')
 image = cv2.imread(file_name) # Read the image with OpenCV
 
 # Get the angles for roll, pitch and yaw
@@ -38,4 +39,4 @@ roll = my_head_pose_estimator.return_roll(image)  # Evaluate the roll angle usin
 pitch = my_head_pose_estimator.return_pitch(image)  # Evaluate the pitch angle using a CNN
 yaw = my_head_pose_estimator.return_yaw(image)  # Evaluate the yaw angle using a CNN
 
-print("Estimated [roll, pitch, yaw] ..... [" + str(roll[0,0,0]) + ", " + str(pitch[0,0,0]) + ", " + str(yaw[0,0,0])  + "]\n")
+print("Estimated [roll, pitch, yaw]:\n[" + str(roll[0,0,0]) + ", " + str(pitch[0,0,0]) + ", " + str(yaw[0,0,0])  + "]\n")
